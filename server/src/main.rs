759c54fab8ec234acb6fa9ae78a8ae0b397d67c6
James Daniel Holby <jamesdholby@gmail.com>
2019-01-20 14:33:09 +0100

    Add sources for GraphQL routing examples

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,6 +12,10 @@ extern crate rocket;
 // Basically just a convenience alias
 use rocket::response::content;
 
+// Seems there's several syntax varieties
+// https://github.com/graphql-rust/juniper/blob/master/juniper_rocket/examples/rocket_server.rs
+// https://github.com/ELD/rocket-juniper-example/blob/master/src/graphql/schema.rs
+// Going with the macro variant as they're basically decorators
 #[get("/")]
 fn graphiql() -> content::Html<String> {
     juniper_rocket::graphiql_source("/graphql")
